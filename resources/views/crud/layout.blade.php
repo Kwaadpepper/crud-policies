935e6c11d34640c563fd0c826c00b2071b219539
Jérémy Munsch <jeremy.munsch@ldc.fr>
2021-07-20 10:41:40 +0200

    Remove route home in default layout

--- resources/views/crud/layout.blade.php
+++ resources/views/crud/layout.blade.php
@@ -23,7 +23,7 @@
         <div id="app">
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="{{ route('home') }}">{{ config('app.name') }}</a>
+                    <a class="navbar-brand" href="/">{{ config('app.name') }}</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
