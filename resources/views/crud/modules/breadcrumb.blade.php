00e5e19a1445a5efa7538751c9c3af8b223b74af
Jérémy Munsch <jeremy.munsch@ldc.fr>
2021-07-26 14:48:53 +0200

    breadcrumb rework with inline-flex

--- resources/views/crud/modules/breadcrumb.blade.php
+++ resources/views/crud/modules/breadcrumb.blade.php
@@ -1,38 +1,48 @@
 @php $params = request()->route()->parameters; @endphp
-@foreach (request()->route()->parameters as $p)
-@if(is_object($p) and in_array(\Illuminate\Database\Eloquent\Model::class, class_parents($p)))
-@can('viewAny', get_class($p))
-<a href="{{ CrudController::getRoutePrefixed("$modelTable.index", [], true, count($params) - $loop->index) }}">{{
-    transFb(
-        sprintf('models.classes.%s', strtolower(Str::plural(class_basename(get_class($p))))),
-        Str::plural(class_basename(get_class($p)))
-    )
-}}</a>&nbsp;-
-@else
-{{
-    transFb(
-        sprintf('models.classes.%s', strtolower(Str::plural(class_basename(get_class($p))))),
-        Str::plural(class_basename(get_class($p)))
-    )
-}}&nbsp;-
-@endcan
-@endif
-@endforeach
-@if(in_array($action, ['viewAny', 'create']))
-@can('viewAny', $modelClass)
-&nbsp;<a href="{{ CrudController::getRoutePrefixed("$modelTable.index") }}">{{
-    transFb(
-        sprintf('models.classes.%s', strtolower(Str::plural(class_basename($modelClass)))),
-        Str::plural(class_basename($modelClass))
-    )
-}}</a>&nbsp;-
-@else
-&nbsp;{{
-    transFb(
-        sprintf('models.classes.%s', strtolower(Str::plural(class_basename($modelClass)))),
-        Str::plural(class_basename($modelClass))
-    )
-}}&nbsp;-
-@endcan
-@endif
-&nbsp;{{ __("crud-policies::crud.$action") }}
+<nav aria-label="breadcrumb" class="d-inline">
+    <ol class="breadcrumb d-inline-flex">
+        @foreach (request()->route()->parameters as $p)
+        @if(is_object($p) and in_array(\Illuminate\Database\Eloquent\Model::class, class_parents($p)))
+        <li class="breadcrumb-item">
+        @can('viewAny', get_class($p))
+            <a href="{{ CrudController::getRoutePrefixed("$modelTable.index", [], true, count($params) - $loop->index) }}">{{
+                transFb(
+                    sprintf('models.classes.%s', strtolower(Str::plural(class_basename(get_class($p))))),
+                    Str::plural(class_basename(get_class($p)))
+                )
+            }}</a>
+        @else
+        <span>{{
+            transFb(
+                sprintf('models.classes.%s', strtolower(Str::plural(class_basename(get_class($p))))),
+                Str::plural(class_basename(get_class($p)))
+            )
+        }}</span>
+        @endcan
+        </li>
+        @endif
+        @endforeach
+        @if(in_array($action, ['viewAny', 'create']))
+        @can('viewAny', $modelClass)
+        <li class="breadcrumb-item">
+            <a href="{{ CrudController::getRoutePrefixed("$modelTable.index") }}">{{
+                transFb(
+                    sprintf('models.classes.%s', strtolower(Str::plural(class_basename($modelClass)))),
+                    Str::plural(class_basename($modelClass))
+                )
+            }}</a>
+        </li>
+        @else
+        <li class="breadcrumb-item">
+        <span>{{
+            transFb(
+                sprintf('models.classes.%s', strtolower(Str::plural(class_basename($modelClass)))),
+                Str::plural(class_basename($modelClass))
+            )
+        }}</span>
+        </li>
+        @endcan
+        @endif
+        <li class="breadcrumb-item active" aria-current="page">{{ __("crud-policies::crud.$action") }}</li>
+    </ol>
+</nav>
