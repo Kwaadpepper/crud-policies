93d38f97e775cd639f8c902bdbf3eb28e509d24a
Jérémy Munsch <jeremy.munsch@ldc.fr>
2021-09-15 10:53:05 +0200

    Fix sorter

--- resources/views/crud/modules/sorter.blade.php
+++ resources/views/crud/modules/sorter.blade.php
@@ -1,4 +1,5 @@
 <tr>
+    @php function __CRUDgenUrl($q) { return url()->current().(!empty(request()->all())?"?":"").http_build_query(array_merge(request()->query(), $q)); } @endphp
     @foreach ($modelClass::getEditableProperties() as $colname => $prop)
     @if(in_array(CrudAction::viewAny(), $prop['actions']))
     @switch($prop['type'])
@@ -9,10 +10,10 @@
             <th scope="col">
                 {{ $prop['label'] }}
                 @if(!session("crud.$modelTable.sort_way") or session("crud.$modelTable.sort_col") !== $colname or (session("crud.$modelTable.sort_way") === 'desc' and session("crud.$modelTable.sort_col") === $colname))
-                <a class="col_sort text-secondary lead asc" href="{{ request()->fullUrlWithQuery(['sort_col' => $colname, 'sort_way' => 'asc']) }}">&nbsp;&darr;&nbsp;</a>
+                <a class="col_sort text-secondary lead asc" href="{{ __CRUDgenUrl(['sort_col' => $colname, 'sort_way' => 'asc']) }}">&nbsp;&darr;&nbsp;</a>
                 @endif
                 @if(!session("crud.$modelTable.sort_way") or session("crud.$modelTable.sort_col") !== $colname or (session("crud.$modelTable.sort_way") === 'asc' and session("crud.$modelTable.sort_col") === $colname))
-                <a class="col_sort text-secondary lead desc" href="{{ request()->fullUrlWithQuery(['sort_col' => $colname, 'sort_way' => 'desc']) }}">&nbsp;&uarr;&nbsp;</a>
+                <a class="col_sort text-secondary lead desc" href="{{ __CRUDgenUrl(['sort_col' => $colname, 'sort_way' => 'desc']) }}">&nbsp;&uarr;&nbsp;</a>
                 @endif
             </th>
     @endswitch
@@ -20,7 +21,7 @@
     @endforeach
     <th scope="col">
         @if(session("crud.$modelTable.sort_way") and session("crud.$modelTable.sort_col"))
-        <a href="{{ request()->fullUrlWithQuery(['rst' => 'true','sort_col' => null, 'sort_way' => null]) }}" class="btn btn-small btn-danger">&#9003;</a>
+        <a href="{{ __CRUDgenUrl(['rst' => 'true','sort_col' => null, 'sort_way' => null]) }}" class="btn btn-small btn-danger">&#9003;</a>
         @endif
     </th>
 </tr>
